925a2c93985dce971066d48aacf62c7bff0f294b
Aditya733101 <bittusarkar733101@gmail.com>
2024-11-02 17:27:34 +0530

    accessModifier'

---
+++ AccessModifier.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Student{
+    public :// memebers of decleared classes public can access to anywere inside of class and access from outside the class
+    string name;
+    int rollNo;
+    float cgpa;
+    // constructor
+
+};
+int main()
+{
+ Student s1;
+ s1.name = "aditya";
+ s1.rollNo = 1;
+ s1.cgpa = 6.2;
+
+}
